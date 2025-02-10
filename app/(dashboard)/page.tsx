05be957c5ffddeea2af73910140d2a07988a316d
Ronnakrit11 <pee_au1@hotmail.com>
2025-02-10 13:31:19 +0700

    done cut stock

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
           <div className="lg:grid lg:grid-cols-12 lg:gap-8">
             <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
               <h1 className="text-3xl font-bold text-gray-900 tracking-tight sm:text-5xl md:text-6xl">
-              ลงทุนทองออนไลน์กับ
+              ออมทองสะสมน้ำหนักกับ
                 <span className="block text-orange-500">ห้างทองมังกรทอง</span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
