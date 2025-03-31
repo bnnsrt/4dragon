fb9cb5a0f7ea8194befeb4e977eba4bb40d8fbd7
nookteanook111 <59superrichsol@gmail.com>
2025-03-31 14:46:36 +0700

    done

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { ArrowRight, CreditCard, Database } from 'lucide-react';
-import { Terminal } from './terminal';
+
 import { GoldChart } from '@/components/GoldChart';
 import { SocialContacts } from '@/components/SocialContacts';
 import { GoldPricesHome } from '@/components/GoldPricesHome';
@@ -39,7 +39,7 @@ export default function HomePage() {
               </div>
             </div>
             <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
-              <Terminal />
+        
             </div>
           </div>
         </div>
